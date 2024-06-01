8b297c25215f961884967ab1762f2ee8451f15f4
ronaldpine <ronaldp10@g.ucla.edu>
2024-06-01 14:08:09 -0700

    Progress commit, implementing udp packet

--- project/client.cpp
+++ project/client.cpp
@@ -4,141 +4,119 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
-#include <csignal> // Used for SIGINT
-#include <fcntl.h> // Used for non-blocking
+#include <csignal>
+#include <fcntl.h>
 
 using namespace std;
 volatile sig_atomic_t status = 1;
 
-// Macros for reliable transport
 #define PAYLOAD_SIZE 1024
 #define HEADER_SIZE 12
 #define PACKET_SIZE (PAYLOAD_SIZE + HEADER_SIZE)
 #define WINDOW_SIZE 20
+#define MSS 1024
 
-void cleanup(int sockfd){
-  close(sockfd);
-  return;
+
+typedef struct __attribute__((__packed__)) {
+    unsigned int seq;
+    unsigned int ack;
+    unsigned short size;
+    char padding[2];
+    char data[MSS];
+} packet;
+
+
+void cleanup(int sockfd) {
+    close(sockfd);
 }
 
-int sendData(int sockfd, string message, struct sockaddr_in serverAddress){
-  char client_buf[1024];
-  strcpy(client_buf, message.c_str());
-  int did_send = sendto(sockfd, client_buf, strlen(client_buf),
-                      0, (struct sockaddr*) &serverAddress,
-                      sizeof(serverAddress));
-  if (did_send < 0) return errno;
-  return 1;
+int sendData(int sockfd, string message, struct sockaddr_in serverAddress) {
+    char client_buf[1024];
+    strcpy(client_buf, message.c_str());
+    int did_send = sendto(sockfd, client_buf, strlen(client_buf), 0, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    if (did_send < 0) {
+        perror("sendto");
+        return errno;
+    }
+    return 1;
 }
 
-
-void processData(){
-  cout << "will process data" << endl;
-  return;
+void processData() {
+    cout << "will process data" << endl;
 }
 
-void retransmit(){
-  return;
+void retransmit() {
+    return;
 }
 
-void timeout(){
-  exit(3);
+void timeout() {
+    exit(3);
 }
 
-void sig(int signal){
-  status = 0;
-   // flush everything to std out
+void sig(int signal) {
+    status = 0;
 }
 
-int main(int argc, char *argv[])
-{
-
-  // Check for valid amount of arguments passed in
-  if(argc != 5){
-    // Not sure about this return code
-    cout << "not enough arguments for client" << endl;
-    return -1;
-  }
-
-
-   // Parse the arguments from the command typed in
-  int securityFlag = stoi(argv[1]);
-  string host = argv[2];
-  string pubKey = argv[4];
-
-
-  // Last ACKed packet
-  int lastAck = 0; 
-  // Last sent packet
-  int lastPacketSent = 0;
-
-  // Used to handle SIGINT
-  signal(SIGINT,sig);
-
-
-  // Create socket
-  int sockfd = socket(AF_INET, SOCK_DGRAM,0);
-
-  // If socket wasn't created properly
-  if(sockfd < 0){
-    exit(errno);
-  }
-
-  // Set up flags so the sokcet is nonblocking
-  int flags = fcntl(sockfd, F_GETFL);
-  flags |= O_NONBLOCK;
-  fcntl(sockfd, F_SETFL, flags);
-
-  // Create the server address with IPV4 and for any connections
-  struct sockaddr_in serverAddress;
-  serverAddress.sin_family = AF_INET;
-  serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-  // Set receiving port and set byte ordering to Big Endian
-  int PORT = stoi(argv[3]);
-  serverAddress.sin_port = htons(PORT); 
-
-  const int trueFlag = 1;
-  if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &trueFlag, sizeof(int)) == -1){
-    cout << "Flag setting issue" << endl;
-    return errno;
-  }
-
-  // send data
-  int dataSent = sendData(sockfd, "Hello world!", serverAddress);
-
-  // Create buffer to store messages
-  int BUF_SIZE = 1024;
-  char server_buf[BUF_SIZE];
-  socklen_t serversize;
-
-
-  // Listen to messages from server
-  while(status == 1){
-    /* 5. Listen for response from server */
-    int bytes_recvd = recvfrom(sockfd, server_buf, BUF_SIZE, 
-                        // socket  store data  how much
-                           0, (struct sockaddr*) &serverAddress, 
-                           &serversize);
-    // If there is data process, else continue
-    // cout << "No data" << endl;
-    if (bytes_recvd > 0){
-    
-    write(1, server_buf, bytes_recvd);
-       // Else process the data 
-    processData();
-
-    // Check if there need to retransmit
-    retransmit();
+int main(int argc, char *argv[]) {
+    if (argc != 5) {
+        cout << "not enough arguments for client" << endl;
+        return -1;
     }
 
+    int securityFlag = stoi(argv[1]);
+    string host = argv[2];
+    string pubKey = argv[4];
 
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        perror("socket");
+        exit(errno);
+    }
 
-    // Print out data
-    sleep(0.5);
-    
-  }
+    int flags = fcntl(sockfd, F_GETFL);
+    flags |= O_NONBLOCK;
+    fcntl(sockfd, F_SETFL, flags);
 
-  return 0;
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    int PORT = stoi(argv[3]);
+    serverAddress.sin_port = htons(PORT);
+
+    const int trueFlag = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &trueFlag, sizeof(int)) == -1) {
+        perror("setsockopt");
+        return errno;
+    }
+
+    int dataSent = sendData(sockfd, "Hello world!", serverAddress);
+    if (dataSent < 0) {
+        perror("sendData");
+    }
+
+    int BUF_SIZE = 1024;
+    char server_buf[BUF_SIZE];
+    socklen_t serverSize = sizeof(serverAddress);
+
+    signal(SIGINT, sig);
+
+    while (status == 1) {
+        memset(server_buf, 0, BUF_SIZE);
+        int bytes_recvd = recvfrom(sockfd, server_buf, BUF_SIZE, 0, (struct sockaddr*)&serverAddress, &serverSize);
+
+        if (bytes_recvd > 0) {
+            write(1, server_buf, bytes_recvd);
+            processData();
+            retransmit();
+        } else if (bytes_recvd < 0 && errno != EAGAIN) {
+            perror("recvfrom");
+        }
+
+        sleep(1); // Wait before checking again to avoid busy waiting
+    }
+
+    cleanup(sockfd);
+    cout << "Client terminated gracefully" << endl;
+    return 0;
 }
-
--- project/server.cpp
+++ project/server.cpp
@@ -3,165 +3,127 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <errno.h>
-#include <csignal> // Used for SIGINT
+#include <csignal>
 #include <cstring>
-#include <fcntl.h> // Used for non-blocking
+#include <fcntl.h>
 
-// Macros for reliable transport
 #define PAYLOAD_SIZE 1024
 #define HEADER_SIZE 12
 #define PACKET_SIZE (PAYLOAD_SIZE + HEADER_SIZE)
 #define WINDOW_SIZE 20
+#define MSS 1024
 
 using namespace std;
 volatile sig_atomic_t status = 1;
 
 
-void cleanup(int sockfd){
-  close(sockfd);
-  return;
+// Struct for packet
+typedef struct __attribute__((__packed__)) {
+    unsigned int seq;
+    unsigned int ack;
+    unsigned short size;
+    char padding[2];
+    char data[MSS];
+} packet;
+
+
+
+void cleanup(int sockfd) {
+    close(sockfd);
 }
 
-void dumpMessage( char client_data[1024], int numBytesRecv){
- cout << "In the dumpMessage function" << endl;
+void dumpMessage(char client_data[], int numBytesRecv) {
+    cout << "In the dumpMessage function" << endl;
     string message(client_data, numBytesRecv);
     cout << "Received message: " << message << endl;
     cout.flush();
-
 }
 
-void sendResponse(int sockfd, struct sockaddr_in clientAddress){
-  char server_buf[] = "Received!";
-  int did_send = sendto(sockfd, server_buf, strlen(server_buf), 
-                   // socket  send data   how much to send
-                      0, (struct sockaddr*) &clientAddress, 
-                   // flags   where to send
-                      sizeof(clientAddress));
-  
-  cout << "Did send value: " << did_send << endl;
-
+int sendData(int sockfd, string message, struct sockaddr_in clientAddress) {
+    char client_buf[1024];
+    strcpy(client_buf, message.c_str());
+    int did_send = sendto(sockfd, client_buf, strlen(client_buf), 0, (struct sockaddr*)&clientAddress, sizeof(clientAddress));
+    if (did_send < 0) {
+        perror("sendto");
+        return errno;
+    }
+    return 1;
 }
 
-void processData(int sockfd,struct sockaddr_in clientAddress,  char client_data[1024], int numBytesRecv){
-
-
-  // Print out the message
-  dumpMessage(client_data, numBytesRecv);
-
-  cout << "Finished dumping message" << endl;
-  // Send response back
-  cout << "Sending Response" << endl;
-  sendResponse(sockfd, clientAddress);
-
-
-
-  return;
+void processData(int sockfd, struct sockaddr_in clientAddress, char client_data[], int numBytesRecv) {
+    dumpMessage(client_data, numBytesRecv);
+    cout << "Finished dumping message" << endl;
+    cout << "Sending Response" << endl;
+    sendData(sockfd, "hello back\n", clientAddress);
 }
 
-void retransmit(){
-  return;
+void retransmit() {
+    return;
 }
 
-void sig(int signal){
-  status = 0;
-  // flush everything to std out
+void sig(int signal) {
+    status = 0;
 }
 
+int main(int argc, char *argv[]) {
+    if (argc != 5) {
+        cout << "Not enough commands " << endl;
+        return -1;
+    }
 
-int main(int argc, char *argv[])
-{
+    int securityFlag = stoi(argv[1]);
+    string privKeyFile = argv[3];
+    string certFile = argv[4];
 
-  // Check for valid amount of arguments passed in
-  if(argc != 5){
-    // Not sure about this return code
-    cout << "Not enough commands " << endl;
-    return -1;
-  }
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        perror("socket");
+        exit(errno);
+    }
 
-  // Parse the arguments from the command typed in
-  int securityFlag = stoi(argv[1]);
-  cout << "Security flag is: " << securityFlag << endl;
-  string privKeyFile = argv[3];
-  string certFile = argv[4];
+    int flags = fcntl(sockfd, F_GETFL);
+    flags |= O_NONBLOCK;
+    fcntl(sockfd, F_SETFL, flags);
 
-  // Last ACKed packet
-  int lastAck = 0; 
-  // Current Ack
-  int currAck = 0;
-  // Last sent packet
-  int currPacket = 1;
-  //
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
 
-  // signal(SIGINT,sig);
+    int PORT = stoi(argv[2]);
+    serverAddress.sin_port = htons(PORT);
 
+    const int trueFlag = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &trueFlag, sizeof(int)) == -1) {
+        perror("setsockopt");
+        return errno;
+    }
 
-  // Create socket
-  int sockfd = socket(AF_INET, SOCK_DGRAM,0);
-  
-  // If socket wasn't created properly
-  if(sockfd < 0){
-    exit(errno);
-  }
+    if (bind(sockfd, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
+        perror("bind");
+        cleanup(sockfd);
+        return errno;
+    }
 
-  // Set up flags so the sokcet is nonblocking
-  int flags = fcntl(sockfd, F_GETFL);
-  flags |= O_NONBLOCK;
-  fcntl(sockfd, F_SETFL, flags);
+    int BUF_SIZE = 1024;
+    char client_buf[BUF_SIZE];
+    struct sockaddr_in clientAddress;
+    socklen_t clientSize = sizeof(clientAddress);
 
-  // Create the server address with IPV4 and for any connections
-  struct sockaddr_in serverAddress;
-  serverAddress.sin_family = AF_INET;
-  serverAddress.sin_addr.s_addr = INADDR_ANY;
+    signal(SIGINT, sig);
 
-  // Set receiving port and set byte ordering to Big Endian
-  int PORT = stoi(argv[2]);
-  cout << "Port number is: " << PORT << endl;
-  serverAddress.sin_port = htons(PORT); 
+    while (status == 1) {
+        // memset(client_buf, 0, BUF_SIZE);
+        int numBytesRecv = recvfrom(sockfd, client_buf, BUF_SIZE, 0, (struct sockaddr*)&clientAddress, &clientSize);
 
-  // Be able to reuse the socket address and port
-  const int trueFlag = 1;
-  if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &trueFlag, sizeof(int)) == -1){
-    // cout << "Flag setting issue" << endl;
-    return errno;
-  }
+        if (numBytesRecv > 0) {
+            processData(sockfd, clientAddress, client_buf, numBytesRecv);
+            retransmit();
+        } else if (numBytesRecv < 0 && errno != EAGAIN) {
+            perror("recvfrom");
+        }
+    }
 
-  // Bind the socket to the os with server address
-  int binded = bind(sockfd, (struct sockaddr*) &serverAddress, 
-                    sizeof(serverAddress));
-
-  // If there was an error binding
-  if (binded < 0){
     cleanup(sockfd);
-    return errno;
-  } 
-
-  // Create buffer for messages from clients
-  int BUF_SIZE = 1024;
-  char client_buf[BUF_SIZE];
-  struct sockaddr_in clientAddress;
-  socklen_t clientSize = sizeof(clientAddress);
-
-
-
-  // Listen to message from clients in non-blocking manner, while running
-  while(status == 1){
-
-    int numBytesRecv = recvfrom(sockfd, client_buf, BUF_SIZE, 0, (struct sockaddr*)&clientAddress, &clientSize);
-
-    if (numBytesRecv > 0) {
-      processData(sockfd, clientAddress, client_buf, numBytesRecv);
-      retransmit();
-    } else if (numBytesRecv < 0 && errno != EAGAIN) {
-      perror("recvfrom");
-    }
-    else{
-      // cout << errno << endl;
-    }
-
-  }
-
-
-
-
-  return 0;
+    cout << "Server terminated gracefully" << endl;
+    return 0;
 }
